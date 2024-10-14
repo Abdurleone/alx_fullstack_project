7fc47b8966f36ab322041e45ba1950873d38a554
Abdurleone <abyyyusuf@gmail.com>
2024-10-14 16:26:46 +0300

    More API configurations

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -17,8 +17,52 @@ router.post("/", async (req,res)=>{
     }
 })
 //UPDATE
+router.put("/:id", async (req,res)=>{
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body})
+        res.status(200).json(updatedHotel)
+    }   catch(err) {
+        res.status(500).json(err)
+    }
+})
 //DELETE
+router.post("/", async (req,res)=>{
+
+    const newHotel = new Hotel(req.body)
+
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
 //GET
+router.post("/", async (req,res)=>{
+
+    const newHotel = new Hotel(req.body)
+
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
 //GET ALL
+router.post("/", async (req,res)=>{
+
+    const newHotel = new Hotel(req.body)
+
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 export default router
