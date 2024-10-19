e765fc0da480aec0f52a6c717306c6f1f9ff830c
Abdurleone <abyyyusuf@gmail.com>
2024-10-20 02:40:38 +0300

    More API configs

--- api/index.js
+++ api/index.js
@@ -6,17 +6,20 @@ import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
 import usersRoute from "./routes/users.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
+//Middlewares
 const app = express()
+
 dotenv.config()
 
 const connect = async ()=>{
     try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to MongoDB")
-  } catch (error) {
-    throw error;
-  }
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        throw error
+    }
 };
 
 // listeners
