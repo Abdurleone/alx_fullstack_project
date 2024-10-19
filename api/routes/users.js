e3e70ea37613ee5facbf6c030c02d54ea6831a3f
Abdurleone <abyyyusuf@gmail.com>
2024-10-19 20:14:50 +0300

    More API Configs

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,13 @@
 import express from "express"
 
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (res,req,next)=>{
+    res.send("hello user, you're authenticated")
+})
 
 //UPDATE
 router.put("/:id", updateUser) 
