bdcb544636fe276a8229624d2f3cee84e7dd62a2
Abdurleone <abyyyusuf@gmail.com>
2024-10-19 17:54:53 +0300

    Creating tokens

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import { createError } from "../utils/error.js"
+import jwt from "jasonwebtoken"
 
 export const register = async (req,res,next)=>{
     try{
@@ -22,18 +24,16 @@ export const register = async (req,res,next)=>{
 
 export const login = async (req,res,next)=>{
     try{
+        const user = await User.findOne({username:req.body.username})
+        if(!user) return next(createError(404, "User not found"))
+        
 
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password, salt);
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if(!isPasswordCorrect) return next(createError(400, "Incorrect Username/Password"))
+        const token = jwt.sign({ id:user._id, isAdmin:user.isAdmin })
 
-        const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:hash,
-        })
-
-        await newUser.save()
-        res.status(200).send("User has been created")
+        const {password, isAdmin, ...otherDetails} = user._doc
+        res.status(200).json({otherDetails})
     }   catch(err){
         next(err)
     }
