bee8e8baf687744f1165795b51af6ce536c99356
Abdurleone <abyyyusuf@gmail.com>
2024-10-19 21:06:03 +0300

    More API Configs

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,32 +7,32 @@ export const updateUser = async (req,res,next)=>{
             { $set: req.body },
             { new: true }
         );
-        res.status(200).json(updatedUser);
+        res.status(200).json(updatedUser)
     } catch (err) {
         next(err);
     }
 }
 export const deleteUser = async (req,res,next)=>{
     try {
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted.");
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted.")
     } catch (err) {
-        next(err);
+        next(err)
     }
 }
 export const getUser = async (req,res,next)=>{
     try {
-        const user = await User.findById(req.params.id);
-        res.status(200).json(user);
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
     } catch (err) {
-        next(err);
+        next(err)
     }
 }
 export const getUsers = async (req,res,next)=>{
     try {
-        const users = await User.find();
-        res.status(200).json(users);
+        const users = await User.find()
+        res.status(200).json(users)
     } catch (err) {
-        next(err);
+        next(err)
     }
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,11 +3,19 @@ import express from "express"
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/checkauthentication", verifyToken, (res,req,next)=>{
-    res.send("hello user, you're authenticated")
-})
+// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+//   res.send("hello user, you are logged in")
+// })
+
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//   res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//   res.send("hello admin, you are logged in and you can delete all accounts")
+// })
 
 //UPDATE
 router.put("/:id", updateUser) 
