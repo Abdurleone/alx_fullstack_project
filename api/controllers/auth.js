6e6e3544103e121e95c586976e105957aed77425
Abdurleone <abyyyusuf@gmail.com>
2024-10-19 18:31:46 +0300

    More API Configs

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -31,14 +31,14 @@ export const login = async (req,res,next)=>{
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if(!isPasswordCorrect) return next(createError(400, "Incorrect Username/Password"))
         
-        const token = jwt.sign({ id:user._id, isAdmin:user.isAdmin }, process.env.JWT)
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
 
         const { password, isAdmin, ...otherDetails } = user._doc
         res.cookie("access_token", token, {
             httpOnly: true,
         })
         .status(200)
-        .json({otherDetails})
+        .json({ ...otherDetails })
     }   catch(err){
         next(err)
     }
