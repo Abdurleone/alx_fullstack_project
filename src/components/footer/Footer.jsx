dec9778952f09edd0fad69a16fe8d0f2aa6283d2
Abdurleone <abyyyusuf@gmail.com>
2024-10-26 02:09:11 +0300

    Amended files

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,17 +5,15 @@ const Footer = () => {
     <div className="footer">
       <div className="fLists">
         <ul className="fList">
-          <li className="fListItem"><a href="https://example.com/customer-service" target="_blank" rel="noopener noreferrer">Customer Service</a></li>
-          <li className="fListItem"><a href="https://example.com/partner-help" target="_blank" rel="noopener noreferrer">Partner Help</a></li>
           <li className="fListItem"><a href="https://abdurleone.com/careers" target="_blank" rel="noopener noreferrer">Careers</a></li>
-          <li className="fListItem"><a href="https://example.com/sustainability" target="_blank" rel="noopener noreferrer">Sustainability</a></li>
-          <li className="fListItem"><a href="https://example.com/press-center" target="_blank" rel="noopener noreferrer">Press center</a></li>
-          <li className="fListItem"><a href="https://example.com/safety-resources" target="_blank" rel="noopener noreferrer">Safety Resource Center</a></li>
+          <li className="fListItem"><a href="https://greenbeltmovement.org" target="_blank" rel="noopener noreferrer">Sustainability</a></li>
+          <li className="fListItem"><a href="https://pexels.com/search/destinations/" target="_blank" rel="noopener noreferrer">Press center</a></li>
+          <li className="fListItem"><a href="https://dignited.com/102891/emergency-services-contacts-kenya/" target="_blank" rel="noopener noreferrer">Safety Resource Center</a></li>
         </ul>
       </div>
       <div className="fText">
         Copyright © 2024 Abdurleone Booking.
-        <a href="https://example.com/terms-and-conditions.pdf" target="_blank" rel="noopener noreferrer"> View Terms and Conditions</a>
+        <a href="https://drive.google.com/file/d/1RKHn3xAFchIQFjmIPpa3Pa2Wp2x86ta-/view?usp=sharing" target="_blank" rel="noopener noreferrer"> View Terms and Conditions</a>
       </div>
     </div>
   );
