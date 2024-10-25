ab6320ed634166d87e5be37c36af1d7f9b7eb949
Abdurleone <abyyyusuf@gmail.com>
2024-10-25 18:07:15 +0300

    Edits

--- api/index.js
+++ api/index.js
@@ -1,62 +1,89 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import authRoute from "./routes/auth.js"
-import hotelsRoute from "./routes/hotels.js"
-import roomsRoute from "./routes/rooms.js"
-import usersRoute from "./routes/users.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-//Middlewares
-const app = express()
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
-const connect = async ()=>{
+// Check if MONGO environment variable is set
+const mongoUri = process.env.MONGO;
+if (!mongoUri) {
+    console.error("MONGO environment variable not set.");
+    process.exit(1);
+}
+
+// Database connection
+const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO)
-        console.log("Connected to MongoDB")
+        await mongoose.connect(mongoUri);
+        console.log("Connected to MongoDB");
     } catch (error) {
-        throw error
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
     }
 };
 
-// listeners
-mongoose.connection.on("disconnected", ()=>{
-    console.log("mongoDB disconnected!!")
-})
+// MongoDB connection listeners
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected!!");
+});
 
-// api setup
-app.get("/", (req,res)=>{
-    res.send("Beginning of something spectacular")
-})
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB Connected!!");
+});
 
-mongoose.connection.on("connected", ()=>{
-    console.log("mongoDB Connected!!")
-})
+// Middleware
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+app.use(cookieParser());
+app.use(express.json());
 
-//middlewares
-app.use(cookieParser())
-app.use(express.json())
+// Logging middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
-app.use("/api/auth", authRoute)
-app.use("/api/hotels", hotelsRoute)
-app.use("/api/rooms", roomsRoute)
-app.use("/api/users", usersRoute)
+// API routes
+app.get("/", (req, res) => {
+    res.send("Beginning of something spectacular");
+});
 
-app.use((err,req,res,next)=>{
-    const errorStatus = err.status || 500
-    const errorMessage = err.message || "something went wrong"
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
     return res.status(errorStatus).json({
-        success:false,
+        success: false,
         status: errorStatus,
         message: errorMessage,
         stack: err.stack,
-    })
-})
+    });
+});
 
-app.listen(2704, ()=>{
-    connect()
-    console.log("Connected to backend!!")
-})
+// Start server
+app.listen(2704, () => {
+    connect();
+    console.log("Connected to backend!!");
+});
+
+// Graceful shutdown
+process.on("SIGINT", async () => {
+    await mongoose.connection.close();
+    console.log("MongoDB connection closed.");
+    process.exit(0);
+});
